909898d6041dcbf4bc6d437af0f54c1487c17c5c
unknown <oh.csharp@gmail.com>
2021-01-29 00:54:18 -0800

    Untrack environment

--- src/environments/environment.prod.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  urlAddress: 'https://api.letspq.com',
-  googleKey: '744982401814-6tfvt0cnj6vvqnspgk9lbsl6nar3h73s.apps.googleusercontent.com',
-  facebookKey: '3542713669169089'
-};
