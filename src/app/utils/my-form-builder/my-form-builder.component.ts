d7eeed7a24e8062abca28da6bb16becc74dc66bb
unknown <oh.csharp@gmail.com>
2021-02-16 04:43:10 -0800

    updating drag and drop

--- src/app/utils/my-form-builder/my-form-builder.component.ts
+++ src/app/utils/my-form-builder/my-form-builder.component.ts
@@ -1,30 +1,50 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
 import {CdkDragDrop, copyArrayItem, moveItemInArray, transferArrayItem} from '@angular/cdk/drag-drop';
 import { FieldConfig, Validator } from "../my-form-builder/components/field.interface";
+import { FormGroup, FormControl } from "@angular/forms"
 
+export interface iDraggable {
+  icon: string,
+  desc: string
+}
 @Component({
   selector: 'app-my-form-builder',
   templateUrl: './my-form-builder.component.html',
   styleUrls: ['./my-form-builder.component.css']
 })
 export class MyFormBuilderComponent implements OnInit {
-  inProgressList: FieldConfig[] = [];
+  inProgressList: iDraggable[] = [ { icon:'input', desc:'Input Text'}];
+  previewList: iDraggable[] = [];
   inflightData: FieldConfig = { name:'hello', label:'label', inputType:'input', options: null, collections: null, type:'string', validations:null, value:"test"};
-  toDoList = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi'
+  toDoList: iDraggable[] = [
+    { icon:'input', desc:'Input Text'},
+    { icon:'file_upload', desc:'Input File'},
+    { icon:'text_snippet', desc:'Input TextArea'},
+    { icon:'check', desc:'Checkbox'},
+    { icon:'add_box', desc:'Select Boxes'},
+    { icon:'view_list', desc:'Select'},
+    { icon:'radio_button_checked', desc:'Button'},
+    { icon:'tag', desc:'Tag'},
+    { icon:'date_range', desc:'Date/Time'},
+    { icon:'content_paste', desc:'Content'},
+    { icon:'padding', desc:'Field Set'},
+    { icon:'crop_portrait', desc:'Panel'},
+    { icon:'filter_center_focus', desc:'Well'},
+    { icon:'view_column', desc:'Columns'},
   ];
+  myGroup = new FormGroup({
+    name: new FormControl(),
+    type: new FormControl()
+ });
+
   constructor() {
   }
 
   drop(event: CdkDragDrop<string[]>) {
-    // console.log(event)
+    console.log(event.previousContainer.data[event.previousIndex])
+    console.log(this.inProgressList);
+    let itemReceived:any = event.previousContainer.data[event.previousIndex]
+    this.previewList.push({icon:itemReceived.icon, desc:itemReceived.desc});
     if (event.previousContainer === event.container) {
       moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
     } else {
@@ -34,6 +54,10 @@ export class MyFormBuilderComponent implements OnInit {
         event.currentIndex);
     }
   }
+  dropPreview(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.previewList, event.previousIndex, event.currentIndex);
+  }
+
   onEntered(enter) {
     console.log('ee', enter);
   }
